3e7477be9285c4180e503769b97de622657c5942
Orstein401 <alerom342@gmail.com>
2025-11-25 16:15:56 +0100

    ver1.4

--- M3-w2-g2/Assets/_project/Scripts/Coin.cs
+++ M3-w2-g2/Assets/_project/Scripts/Coin.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-
+    [SerializeField] int amount;
    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var comp= collision.GetComponent<CoinHolder>();
         if (comp)
         {
-            comp.coin += 1;
+            comp.coin += amount;
             Debug.Log("le monete sono " + comp.coin);
         }
         Destroy(gameObject);
